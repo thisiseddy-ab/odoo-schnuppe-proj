90289a85f17e92591099bd4b64c83fd51bc1d562
Edin Abdiu <edinabdiu@gmail.com>
2025-04-28 22:42:11 +0200

    Filtered Products

--- addons/product_management/controllers/website_sale_extend.py
+++ addons/product_management/controllers/website_sale_extend.py
@@ -1,22 +1,40 @@
 
-from addons.website_sale.controllers.main import WebsiteSale
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo import http
 from odoo.http import request
+from odoo.osv import expression
+
+'''
+## Test Log##
+import logging
+_logger = logging.getLogger(__name__)
+'''
 
 class WebsiteSaleExtend(WebsiteSale):
 
-    def _get_search_domain(self, search, category, attrib_values, **post):
-        domain = super()._get_search_domain(search, category, attrib_values, **post)
-
-        website = request.env['website'].get_current_website()
-        shop_type = website.shop_type
+    @http.route('/b2c/shop', type='http', auth="public", website=True)
+    def b2c(self, page=0, category=None, search='', **post):
+        """ Override the shop route to add a custom filter for B2C """
         
-        if shop_type:
-            domain += [
-                ('shop_visibility', '=', True),
-                ('shop_type', '=', shop_type),
-            ]
-        else:
-            # Optional: fallback if no shop_type is set
-            domain += [('shop_visibility', '=', True)]
+        request.update_context(shop_type='b2c')
+        return super(WebsiteSaleExtend, self).shop(page=page, category=category, search=search, **post)
 
-        return domain
+    @http.route('/b2b/shop', type='http', auth="public", website=True)
+    def b2b(self, page=0, category=None, search='', **post):
+        """ Override the shop route to add a custom filter for B2B """
+        
+        request.update_context(shop_type='b2b')
+        return super(WebsiteSaleExtend, self).shop(page=page, category=category, search=search, **post)
+
+    def _shop_lookup_products(self, attrib_set, options, post, search, website):
+        """ Override Odoo's internal product search logic to filter by buisness_type """
+
+        # Standard behavior first
+        fuzzy_search_term, product_count, search_product = super()._shop_lookup_products(attrib_set, options, post, search, website)
+
+        # Now apply your custom filter
+        shop_type = request.context.get('shop_type')
+        if shop_type:
+            search_product = search_product.filtered(lambda p: p.shop_type == shop_type and p.shop_visibility)
+
+        return fuzzy_search_term, len(search_product), search_product
